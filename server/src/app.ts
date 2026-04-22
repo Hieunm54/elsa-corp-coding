e23ddef7cf1ea6440bf8f4df66a2a5f705cf2c0a
hieunm54 <hieunm.542@gmail.com>
2026-04-22 21:12:58 +0700

    fix cors

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import healthRouter from "./routes/health";
 import quizzesRouter from "./routes/quizzes";
-
+import cors from "cors";
 const app = express();
 
+app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/", healthRouter);
 app.use("/quizzes", quizzesRouter);
